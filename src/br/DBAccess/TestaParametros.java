3bf497da5c063bd2890bc78055b7ffc3b4518ade
Carlos <cszneider@gmail.com>
2018-10-10 20:49:40 -0300

    Criação de classes Singleton

---
+++ src/br/DBAccess/TestaParametros.java
@@ -0,0 +1,15 @@
+package br.DBAccess;
+
+import java.io.IOException;
+
+public class TestaParametros {
+	
+	public static void main(String[] args) throws IOException {
+			
+		Parametros prm = Parametros.getInstance();
+		
+		System.out.println( "URL Banco: " + prm.getParametro( "urlBanco" ) );
+		System.out.println( "CNPJ     : " + prm.getParametro( "cnpj" ) );
+		System.out.println( "Nro Sala : " + prm.getParametro( "nrSala" ) );
+	}
+}
